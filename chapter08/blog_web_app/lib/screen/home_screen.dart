a8c5460f737fde1896e14a56c19209c29b574708
ragnar-park <mystparkkorea@gmail.com>
2024-03-27 12:20:37 +0900

    [블로그 웹 앱] 앱바 구현

--- chapter08/blog_web_app/lib/screen/home_screen.dart
+++ chapter08/blog_web_app/lib/screen/home_screen.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class HomeScreen extends StatelessWidget {
+  WebViewController webViewController = WebViewController();
 
   // const 생정자
-  const HomeScreen({Key? key}) : super(key: key);
+  HomeScreen({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // 앱바 위젯 추가
+      appBar: AppBar(
+
+        // 배경색 지정
+        backgroundColor: Colors.orange,
+
+        // 앱 타이틀 성정
+        title: Text('Ragnar'),
+
+        // 가운데 정렬
+        centerTitle: true,
+
+      ),
       body: Text('Home Screen'),
     );
   }
