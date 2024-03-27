25614496b6168fb09d13cc0bf29fd1ebedaafc46
ragnar-park <mystparkkorea@gmail.com>
2024-03-27 16:23:39 +0900

    [블로그 웹 앱] 홈 버튼 구현

--- chapter08/blog_web_app/lib/screen/home_screen.dart
+++ chapter08/blog_web_app/lib/screen/home_screen.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
+// URI/URL을 생성하는데 도움을 주는 클래스
+final uri = Uri.parse('https://blog.codefactory.ai');
+
 class HomeScreen extends StatelessWidget {
   // 웹뷰컨트롤러 선언
   WebViewController webViewController = WebViewController()
 
   // webViewController의 loadRequest() 함수를 실행
-  ..loadRequest(Uri.parse('https://blog.codefactory.ai'))
+  ..loadRequest(uri)
 
   // JavaScript가 제한 없이 실행될 수 있도록 함
   ..setJavaScriptMode(JavaScriptMode.unrestricted);
@@ -26,6 +29,19 @@ class HomeScreen extends StatelessWidget {
 
         // 가운데 정렬
         centerTitle: true,
+
+        // AppBar에 액션 버튼을 추가할 수 있는 매개변수
+        actions: [
+          IconButton(
+              // 아이콘을 눌렀을 때 실행할 콜백 함수
+              onPressed: () {
+                webViewController.loadRequest(uri);
+              },
+              icon: Icon(
+                Icons.home
+              ),
+          )
+        ],
 
       ),
       body: WebViewWidget(
