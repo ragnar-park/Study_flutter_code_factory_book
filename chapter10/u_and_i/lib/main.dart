6ec3029fbffb4c4a32cfcd11d936741f44f2a930
ragnar-park <mystparkkorea@gmail.com>
2024-03-29 12:17:05 +0900

    [만난 지 며칠 U&I] 레이아웃 구현

--- chapter10/u_and_i/lib/main.dart
+++ chapter10/u_and_i/lib/main.dart
@@ -4,7 +4,31 @@ import 'package:u_and_i/screen/home_screen.dart';
 void main() {
   runApp(
     MaterialApp(
+      theme: ThemeData(  // 테마를 지정할 수 있는 클래스
+          fontFamily: 'sunflower',  // 기본 글씨체
+          textTheme: TextTheme(     // 글짜 테마를 적용할 수 있는 클래스
+            headline1: TextStyle(   //  headline1 스타일 정의
+              color: Colors.white,  //  글 색상
+              fontSize: 80.0,       //  글 크기
+              fontWeight: FontWeight.w700, //  글 두께
+              fontFamily: 'parisienne',    //  글씨체
+            ),
+            headline2: TextStyle(
+              color: Colors.white,
+              fontSize: 50.0,
+              fontWeight: FontWeight.w700,
+            ),
+            bodyText1: TextStyle(
+              color: Colors.white,
+              fontSize: 30.0,
+            ),
+            bodyText2: TextStyle(
+              color: Colors.white,
+              fontSize: 20.0,
+            ),
+          )
+      ),
       home: HomeScreen(),
-    )
+    ),
   );
 }
--- chapter10/u_and_i/lib/screen/home_screen.dart
+++ chapter10/u_and_i/lib/screen/home_screen.dart
@@ -6,7 +6,91 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Home Screen'),
+      backgroundColor: Colors.pink[100],
+
+      // 시스템 UI 피해서 UI 그리기
+      body: SafeArea(
+        top: true,
+        bottom: false,
+        child: Column(
+          // 위아레 끝에 위젯 배치
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+          // 반대쪽 최대 크기로 늘리기
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            _DDAY(),
+            _CoupleImage(),
+          ],
+        ),
+      ),
     );
   }
+}
+
+class _DDAY extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // 테마 불러오기
+    final textTheme = Theme.of(context).textTheme;
+
+    return Column(
+      children: [
+        const SizedBox(height: 16.0),
+        Text( // 최상단 U&I 글자
+          'U&I',
+          style: textTheme.headline1,
+        ),
+        const SizedBox(height: 16.0),
+        Text( // 두 번째 글자
+          '우리 처음 만난 날',
+          style: textTheme.bodyText1,
+        ),
+        Text( // 임시로 지정한 만난 날짜
+          '1998. 03. 07',
+          style: textTheme.bodyText2,
+        ),
+        const SizedBox(height: 16.0),
+        IconButton( // 하트 아이콘 버튼
+            iconSize: 60.0,
+            onPressed: () {},
+            icon: Icon(
+              Icons.favorite,
+              color: Colors.red,
+            )
+        ),
+        const SizedBox(height: 16.0),
+        Text(
+          'D+365',
+          style: textTheme.headline2,
+        )
+      ],
+    );
+  }
+}
+
+class _CoupleImage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // 오버플로 대응을 위한 Expanded추가
+    // Expanded 위젯은 컨테이너의 크기에 맞게 위젯의 크기를 조절함, 컨테이너가 수평 방향이면 위젯의 너비를, 수직 방향이면 높이를 조절
+    // 이미지가 남는 공간인만큼만 차지하도록 됨
+    return Expanded(
+        child: Center( // 이미지 중앙 정렬
+          child: Image.asset(
+          'asset/img/middle_image.png',
+
+          // 화면의 반만큼 높이 구현
+          // of 생성자
+          // of(context)로 정의된 모든 생성자는 일반적으로 BuildContext를 매개변수로 받고 위젯 트리에서 가장 가까이 있는 객체의 값을 찾아냄
+          // MediaQuery.of(context)는 현재 위젯 트리에서 가장 가까이 있는 MediaQuery값을 찾아냄
+          // 1. 앱 실행시 MaterialApp이 빌드됨과 동시에 MediaQuery 생성
+          // 2. 위젯 트리 아래에서 MediaQuery.of(context)를 실행하면 위젯 트리를 올라가며 가장 가까운 곳에 MediaQuery값을 가져옴
+          // Expanded가 우선순위를 갖게 되어 무시 당함
+          height: MediaQuery.of(context).size.height / 2,
+          ),
+        ),
+    );
+  }
+
 }
