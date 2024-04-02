bd9499955077cdc44f97f53752f72595f9a5457c
ragnar-park <mystparkkorea@gmail.com>
2024-04-02 16:54:01 +0900

    [동영상 플레이어] renderEmpty 함수 구현

--- chapter12/vid_player/lib/screen/home_screen.dart
+++ chapter12/vid_player/lib/screen/home_screen.dart
@@ -1,12 +1,87 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _HomeScreen();
+}
+
+class _HomeScreen extends State<HomeScreen> {
+  XFile? video; // 동영상 저장할 변수
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Home Screen'),
+      backgroundColor: Colors.black,
+      body: video == null ? renderEmpty() : renderVideo(),
+    );
+  }
+
+  // 동영상 선택 전 보여줄 위젯
+  Widget renderEmpty() {
+    return Container(
+      width: MediaQuery.of(context).size.width, // 너비 최대로 늘려주기
+      child: Column(
+        // 위젯들 가운데 정렬
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          _Logo(), // 로고 이미지
+          SizedBox(height: 30.0),
+          _AppHome(), // 앱 이름
+        ],
+      ),
+    );
+  }
+
+  // 동영상 선택 후 보여줄 위젯
+  Widget renderVideo() {
+    return Container();
+  }
+}
+
+// 로고를 보여줄 위젯
+class _Logo extends StatelessWidget {
+  const _Logo({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      'asset/img/logo.png', // 로고 이미지
+    );
+  }
+}
+
+// 앱 제목을 보여줄 위젯
+class _AppHome extends StatelessWidget {
+  const _AppHome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyle = TextStyle(
+      color: Colors.white,
+      fontSize: 30.0,
+      fontWeight: FontWeight.w300,
+    );
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center, // 글자 가운데 정렬
+      children: [
+        Text(
+          'VIDEO',
+          style: textStyle,
+        ),
+        Text(
+          'PLAYER',
+          style: textStyle.copyWith(
+            // textStyle에서 두깨만 700으로 변경
+            fontWeight: FontWeight.w700,
+          ),
+        )
+      ],
     );
   }
 }
