143322deca2430795d96071790ad53e2a3705d49
ragnar-park <mystparkkorea@gmail.com>
2024-04-16 12:39:29 +0900

    [오늘도 출첵] body 구현

--- chapter14/chool_check/lib/screen/home_screen.dart
+++ chapter14/chool_check/lib/screen/home_screen.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomeScreen extends StatelessWidget {
+  static final LatLng companyLatLng = LatLng(37.5233273, 126.921252);
+
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: renderAppBar(),
-      body: Text('Home Screen'),
+      body: GoogleMap(
+        initialCameraPosition:
+            //  CameraPosition은 카메라의 위치와 방향을 나타내는 클래스
+            CameraPosition(target: companyLatLng, zoom: 16 // 확대 정도
+                ),
+      ),
     );
   }
 
